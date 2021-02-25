fe0d91223a6777e672262606f0dde629ffcabdea
Marcus Johnson <darthbison@gmail.com>
2021-02-25 06:50:40 -0500

    Add c# examples

---
+++ Console/Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Calculations
+            var x = System.Math.Floor(14.56);
+            var y = System.Math.Sqrt(100);
+            #endregion
+
+
+            Console.WriteLine("X: " + x);
+            Console.WriteLine("Y: " + y);
+
+            Console.ReadKey(true);
+        }
+    }
+}
